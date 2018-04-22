97604dd0a8793d004bf46a082ad586a60a582b62
Matheus Tadeu <mtqr1@hotmail.com>
2018-04-22 15:08:16 -0300

    Correção do Portfolio

--- js/init.js
+++ js/init.js
@@ -6,3 +6,17 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function (&quot;return false&quot;)
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
+
+window.onbeforeunload = function(){ window.scrollTo(0,0); }
